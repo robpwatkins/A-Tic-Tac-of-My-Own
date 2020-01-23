cfac5fc2101b9b8939394fb2b513e2668d6118bb
Rob W <robpwatkins@gmail.com>
2020-01-23 12:30:39 -0600

    yes

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 document.querySelectorAll('.box').forEach(box => box.addEventListener('click', clicked));
+document.querySelector('button').addEventListener('click', buttonClicked);
 
 function clicked (event) {
   event.target.innerHTML = 'X';
+}
+
+function buttonClicked () {
+  const box = document.querySelectorAll('.box');
+  box.style.background = 'white'
 }
